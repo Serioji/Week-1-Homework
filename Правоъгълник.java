dc332b931e2e31ea6292ced57c5c77b9c2a616df
Serioji <63392487+Serioji@users.noreply.github.com>
2020-04-17 00:22:26 +0300

    Add files via upload

---
+++ Правоъгълник.java
@@ -0,0 +1,24 @@
+public class Правоъгълник {
+
+
+    public static void main(String[] args) {
+    Perimeter();
+    Area();
+
+
+    }
+    public static void Perimeter() {
+        int a = 2;
+        int b = 6;
+        int P = 2*a + 2*b;
+        System.out.println("Периметъра на правоъгълника е : " + P);
+
+
+    }
+    public static void Area() {
+        int a = 2;
+        int b = 6;
+        int S = a*b;
+        System.out.println("Лицето на Правоъгълника е :"+ S);
+    }
+}
