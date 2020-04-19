b084d7ba2eb75b111aa3c3e745172f7363081922
Serioji <63392487+Serioji@users.noreply.github.com>
2020-04-19 12:29:57 +0300

    Add files via upload

---
+++ month.java
@@ -0,0 +1,60 @@
+package mat;
+
+public class month {
+
+    public static void main(String[] args) {
+        Monthnum();
+
+
+
+
+    }
+
+    public static void Monthnum () {
+        int monthnumber = 1;
+
+        if(monthnumber == 1){
+            System.out.println("Месец Януари има 31 дена");
+        }
+        else if(monthnumber == 2){
+            System.out.println("Месец Февруари има или 28 или 29 дена зависещо от годината");
+        }
+        else if(monthnumber == 3) {
+            System.out.println("Месец Март има 31 дена");
+        }
+        else if(monthnumber == 4){
+            System.out.println("Месец Април има 30 дена");
+        }
+        else if(monthnumber == 5){
+            System.out.println("Месец Май има 31 дена");
+        }
+        else if(monthnumber == 6){
+            System.out.println("Месец Юни има 30 дена");
+        }
+        else if(monthnumber == 7){
+            System.out.println("Месец Юли има 31 дена");
+        }
+        else if(monthnumber == 8){
+            System.out.println("Месец Август има 31 дена");
+        }
+        else if(monthnumber == 9){
+            System.out.println("Месец Септември има 30 дена");
+        }
+        else if(monthnumber == 10){
+            System.out.println("Месец Октомври има 31 дена");
+        }
+        else if(monthnumber == 11){
+            System.out.println("Месец Ноември има 30 дена");
+        }
+        else if(monthnumber == 12){
+            System.out.println("Месец Декември има 31 дена");
+        }
+
+
+
+
+
+
+
+    }
+}
